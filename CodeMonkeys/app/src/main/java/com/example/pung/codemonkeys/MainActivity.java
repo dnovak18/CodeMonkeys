3f08217d7afa10f8a65a799a3c22bea6ed40b6ed
Unknown <jz6483at@metrostate.edu>
2018-02-27 20:49:10 -0600

    name under icon bug fix

--- CodeMonkeys/app/src/main/java/com/example/pung/codemonkeys/MainActivity.java
+++ CodeMonkeys/app/src/main/java/com/example/pung/codemonkeys/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.pung.codemonkeys;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -12,6 +14,7 @@ import android.database.SQLException;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -52,8 +55,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
-        setSupportActionBar(toolbar);
 
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
         //////////////////////////////////////////////////////
         /*
         Button loginButton = (Button) findViewById(R.id.loginButton);
@@ -108,7 +112,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void infoClick(View view) {
-        Toast.makeText(MainActivity.this, "Cheeeeers!", Toast.LENGTH_LONG).show();
+
+        Toast toast = Toast.makeText(MainActivity.this, "Cheeeeers!", Toast.LENGTH_SHORT);
+        TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
+        v.setTextColor(Color.RED);
+        v.setTextSize(40);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
+
+        //Toast.makeText(MainActivity.this, "Cheeeeers!", Toast.LENGTH_LONG).show();
+
 
     }
     /////////////////////////////////////////////
