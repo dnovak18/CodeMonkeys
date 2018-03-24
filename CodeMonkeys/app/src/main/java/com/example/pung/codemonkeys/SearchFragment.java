2544532fbb9b7b327c00a9809fa0c4e332f18a95
Prathna <jz6483at@metrostate.edu>
2018-03-23 21:59:10 -0500

    search autocomplete done

--- CodeMonkeys/app/src/main/java/com/example/pung/codemonkeys/SearchFragment.java
+++ CodeMonkeys/app/src/main/java/com/example/pung/codemonkeys/SearchFragment.java
@@ -1,13 +1,18 @@
 package com.example.pung.codemonkeys;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.AppCompatAutoCompleteTextView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -24,6 +29,9 @@ public class SearchFragment extends Fragment {
     String breweryCityText;
     String beerTypeText;
     String beerNameText;
+    DatabaseHelper myDbHelper;
+    String breweryNameResult;
+   // String[] arrayBreweryName;
     public interface ChangeFragment{
         public void setFrag();
     }
@@ -36,6 +44,25 @@ public class SearchFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
 
+    }
+    //autocomplete method, takes colum name and table name, return string array
+    public String[] getArrayBreweryName(String search, String table){
+
+        String searchDatabase = "SELECT DISTINCT "+search+" FROM  "+table;
+        myDbHelper = new DatabaseHelper(getContext());
+        myDbHelper.openDataBase();
+        SQLiteDatabase db = myDbHelper.getWritableDatabase();
+        Cursor cursor = db.rawQuery(searchDatabase,null);
+        String[] databaseResult = new String[cursor.getCount()];
+        if(cursor.moveToFirst()){
+            databaseResult[0] = cursor.getString(0);
+        }
+        for(int i = 1; i<databaseResult.length;i++){
+            if(cursor.moveToNext()==true){
+                databaseResult[i] = cursor.getString(0);
+            }
+        }
+        return databaseResult;
     }
 
     @Override
@@ -50,8 +77,32 @@ public class SearchFragment extends Fragment {
         beerType = (EditText) view.findViewById(R.id.beerTypeEditText);
         beerName = (EditText) view.findViewById(R.id.beerNameEditText);
 
-          Button searchButton = (Button) view.findViewById(R.id.searchButton);
-         searchButton.setOnClickListener(new View.OnClickListener() {
+        //autocomplete
+        AutoCompleteTextView autoCompleteTextViewBreweryName = (AutoCompleteTextView)view.findViewById(R.id.breweryNameEditText);
+        ArrayAdapter<String> adapterBreweryName = new ArrayAdapter<String>(getActivity(),R.layout.support_simple_spinner_dropdown_item,getArrayBreweryName("brewery_name", "brewery_table"));
+        autoCompleteTextViewBreweryName.setAdapter(adapterBreweryName);
+
+        AutoCompleteTextView autoCompleteTextViewBreweryZip = (AutoCompleteTextView)view.findViewById(R.id.breweryZipEditText);
+        ArrayAdapter<String> adapterBreweryZip = new ArrayAdapter<String>(getActivity(),R.layout.support_simple_spinner_dropdown_item,getArrayBreweryName("brewery_zip", "brewery_table"));
+        autoCompleteTextViewBreweryZip.setAdapter(adapterBreweryZip);
+
+        AutoCompleteTextView autoCompleteTextViewBreweryCity = (AutoCompleteTextView)view.findViewById(R.id.breweryCityEditText);
+        ArrayAdapter<String> adapterBreweryCity = new ArrayAdapter<String>(getActivity(),R.layout.support_simple_spinner_dropdown_item,getArrayBreweryName("brewery_city", "brewery_table"));
+        autoCompleteTextViewBreweryCity.setAdapter(adapterBreweryCity);
+
+        AutoCompleteTextView autoCompleteTextViewBeerType = (AutoCompleteTextView)view.findViewById(R.id.beerTypeEditText);
+        ArrayAdapter<String> adapterBeerType = new ArrayAdapter<String>(getActivity(),R.layout.support_simple_spinner_dropdown_item,getArrayBreweryName("beer_type", "beer_table"));
+        autoCompleteTextViewBeerType.setAdapter(adapterBeerType);
+
+        AutoCompleteTextView autoCompleteTextViewBeerName = (AutoCompleteTextView)view.findViewById(R.id.beerNameEditText);
+        ArrayAdapter<String> adapterBeerName = new ArrayAdapter<String>(getActivity(),R.layout.support_simple_spinner_dropdown_item,getArrayBreweryName("beer_name", "beer_table"));
+        autoCompleteTextViewBeerName.setAdapter(adapterBeerName);
+
+
+
+        //search
+        Button searchButton = (Button) view.findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
           @Override
     public void onClick(View view) {
 
