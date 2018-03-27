a286cbf33a6f4be11e303cc1b86031cbc59f1611
nclanners <nclanners@gmail.com>
2018-03-27 13:57:58 -0500

    Top and bottom menus are in

--- CodeMonkeys/app/src/main/java/com/example/pung/codemonkeys/MapsActivity.java
+++ CodeMonkeys/app/src/main/java/com/example/pung/codemonkeys/MapsActivity.java
@@ -1,14 +1,20 @@
 package com.example.pung.codemonkeys;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.*;
-
+import android.support.v7.widget.Toolbar;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -103,11 +109,38 @@ public class MapsActivity extends AppCompatActivity implements
      */
     private boolean mPermissionDenied = false;
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.setting_menu_items, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.login:
+                Intent intent = new Intent(MapsActivity.this, LoginActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.logout:
+                break;
+            case R.id.about:
+                getSupportFragmentManager().beginTransaction().replace(R.id.activity_main, new AboutDeveloperFragment()).addToBackStack(null).commit();
+                break;
+            default:
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             checkLocationPermission();
@@ -127,6 +160,43 @@ public class MapsActivity extends AppCompatActivity implements
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+    }
+
+    public void scanClick(MenuItem item) {
+        // Intent scanClick = new Intent(MainActivity.this, ScanActivity.class);
+        // startActivity(scanClick);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.activity_main, new ScannerFragment()).addToBackStack(null).commit();
+    }
+
+
+
+    public void searchClick(MenuItem item) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.activity_main, new  SearchFragment()).addToBackStack(null).commit();
+
+    }
+    public void mapClick(MenuItem item) {
+        Intent mapClick = new Intent(MapsActivity.this, MapsActivity.class);
+        startActivity(mapClick);
+    }
+
+    public void profileClick(MenuItem item) {
+//        getSupportFragmentManager().beginTransaction().replace(R.id.activity_main, new MyProfileFragment()).addToBackStack(null).commit();
+        Intent profileClick = new Intent(MapsActivity.this, LoginActivity.class);
+        startActivity(profileClick);
+    }
+    public void infoClick(View view) {
+
+        Toast toast = Toast.makeText(MapsActivity.this, "Cheeeeers!", Toast.LENGTH_SHORT);
+        TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
+        v.setTextColor(Color.RED);
+        v.setTextSize(40);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
+
+        //Toast.makeText(MainActivity.this, "Cheeeeers!", Toast.LENGTH_LONG).show();
 
 
     }
@@ -420,4 +490,5 @@ public class MapsActivity extends AppCompatActivity implements
         PermissionUtils.PermissionDeniedDialog
                 .newInstance(true).show(getSupportFragmentManager(), "dialog");
     }
+
 }
